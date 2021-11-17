98956ba817a372658008486510568253901447e6
Pozsgai Barnabás <h050515@stud.u-szeged.hu>
2021-11-17 15:25:20 +0100

    detailsFunction

--- admin.php
+++ admin.php
@@ -1,6 +1,18 @@
 <?php
-    include "class/Admin.php";
-    $person = new Admin();
+
+if (isset($_POST["details"])) {
+    
+    $userId = $_POST['detailsId'];
+    $url = "person.php?userId=$userId";
+    header("Location: $url");
+
+}
+if (isset($_POST["modify"])) {
+    echo "aaaaa";
+} else {
+    echo "Hiba!";
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -13,10 +25,78 @@
     <link rel="stylesheet" href="css/admin.css">
 </head>
 <body>
+    <div class="title">
+        <h1>Game Of Thrones</h1>
+    </div>
     <div class="content">
         <?php
-            $person->getPersonData();
+            $admin = new Admin();
+            $admin->listPersonData();
         ?>
     </div>
 </body>
 </html>
+
+<?php
+class Admin {
+
+	private $servername = "localhost";
+	private $username = "root";
+	private $password = "";
+	private $dbname = "thrones";
+	private $conn = NULL;
+
+    public function __construct(){
+		
+		$this->conn = new mysqli(
+            $this->servername, 
+            $this->username, 
+            $this->password, 
+            $this->dbname);
+
+		if ($this->conn->connect_error) {
+    		die("Kapcsolati hiba: " . $conn->connect_error);
+		} 
+	}
+
+    public function __destruct(){
+		$this->conn->close();
+	}
+
+    public function listPersonData() {
+        
+        $sql = "SELECT * FROM person";
+        $result = $this->conn->query($sql);
+
+        echo "<table>";
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                
+                echo "<tr><td>";
+                    echo "<div class='person'>";
+                        echo "<form method='POST' action='". $_SERVER['PHP_SELF'] ."'>";
+                            echo "<img src='img/" . $row["picture"]. "'>";
+                            echo "<input type='text' name='name' value='" . $row["name"] . "'>";
+                            echo "<input type='number' name='age' value='" .  $row["age"] . "'>";
+                            echo "<input type='text' name='title' value='" . $row["title"]. "'>";
+                            echo "<input type='text' name='House_name' value='" . $row["House_name"]. "'>";
+                            echo "<input type='hidden' name='modifyId' value='" . $row['id'] ."'>";
+                            echo "<input type='submit' value='Modify' name='modify' id='modifyButton'>";
+                        echo "</form>";
+                        echo "<form method='POST' action='". $_SERVER['PHP_SELF'] ."'>";
+                            echo "<input type='hidden' name='detailsId' value='" . $row['id'] ."'>";
+                            echo "<input type='submit' value='Details' name='details' id='detailsButton'>";
+                        echo "</form>";
+                    echo "</div>";
+                echo "</td></tr>";
+            }
+        } else {
+            echo "0 results";
+        }
+        echo "</table>";        
+    }
+}
+
+
+
+?>
--- person.php
+++ person.php
@@ -1,9 +1,5 @@
 <?php
-    include "class/Person.php";
-    $person = new Person();
-    $personData = $person->getPersonDataById(1);
-    /*INSERT INTO `person`( `name`, `age`, `title`, `House.name`, `picture`)
-            VALUES ('Jamie', 44, 'first-born', 'Lannister', 'jamie.PNG')*/
+
 ?>
 
 <!DOCTYPE html>
@@ -17,6 +13,10 @@
     <title>Game Of Thrones</title>
 </head>
 <body>
+    <?php
+        $person = new Person();
+        $personData = $person->getPersonDataById($_GET["userId"]);
+    ?>
     <div class="content">
         <div class="container">
             <div class="fixed">
@@ -28,13 +28,55 @@
                     <?php 
                         echo " age: " . $personData["age"] .  "<br />" .
                             " title: " . $personData["title"] .  "<br />" .
-                            " House name: " . $personData["House.name"];
+                            " House name: " . $personData["House_name"];
                     ?>
                 </p>
                 <input type="submit" value="Back" name="back">
             </div>
         </div> 
- 
     </div>
 </body>
 </html>
+
+<?php
+class Person {
+
+	private $servername = "localhost";
+	private $username = "root";
+	private $password = "";
+	private $dbname = "thrones";
+	private $conn = NULL;
+
+    public function __construct(){
+		
+		$this->conn = new mysqli(
+            $this->servername, 
+            $this->username, 
+            $this->password, 
+            $this->dbname);
+
+		if ($this->conn->connect_error) {
+    		die("Kapcsolati hiba: " . $conn->connect_error);
+		} 
+	}
+
+    public function __destruct(){
+		$this->conn->close();
+	}
+
+    public function getPersonDataById($id) {
+        
+        $sql = "SELECT * FROM person WHERE id =" . $id;
+        $result = $this->conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            
+            $row = $result->fetch_assoc();
+            return $row;
+            
+        } else {
+            echo "Nincs eredmény";
+        }
+    }
+}
+?>
