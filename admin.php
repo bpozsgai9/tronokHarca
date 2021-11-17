4e0d33db122afdaa9b4641e060ed3f7fd9e78a57
Pozsgai Barnabás <h050515@stud.u-szeged.hu>
2021-11-17 17:51:29 +0100

    more

--- admin.php
+++ admin.php
@@ -1,4 +1,5 @@
 <?php
+$admin = new Admin();
 
 if (isset($_POST["details"])) {
     
@@ -9,6 +10,15 @@ if (isset($_POST["details"])) {
 }
 if (isset($_POST["modify"])) {
     echo "aaaaa";
+}
+
+if (isset($_POST["create"])) {
+    $admin->insertPerson(
+        $_POST['name'], 
+        $_POST['age'], 
+        $_POST['title'], 
+        $_POST['House_name']
+    );
 }
 
 if (isset($_POST["familyTree"])) {
@@ -34,8 +44,31 @@ if (isset($_POST["familyTree"])) {
         </form>
     </div>
     <div class="content">
+        <div class='person'>
+            <!---<form method="POST"  action="<?php echo $_SERVER['PHP_SELF']; ?>"enctype="multipart/form-data">
+                    <input type="file" name="fileToUpload" id="fileToUpload">
+                    <input type="submit" value="Feltölt" name="submit"><br>
+                </form>-->
+                <?php/*
+                    
+
+                    //bef
+                    if (!empty($_POST["submit"]) && isset($_POST["submit"])) {
+    
+                        $admin->uploadFile();
+                    }*/
+                ?>
+            <form method='POST' action=<?php $_SERVER['PHP_SELF'] ?>>
+                New Person:
+                <img src=''>
+                <input type='text' name='name' value='' placeholder='Name'>
+                <input type='number' name='age' value='' placeholder='Age'>
+                <input type='text' name='title' value='' placeholder='Title'>
+                <input type='text' name='House_name' value='' placeholder='House Name'>
+                <input type='submit' value='Create' name='create' id='createButton'>
+            </form>
+        </div>
         <?php
-            $admin = new Admin();
             $admin->listPersonData();
         ?>
     </div>
@@ -99,6 +132,53 @@ class Admin {
             echo "0 results";
         }
         echo "</table>";        
+    }
+
+    public function uploadFile() {
+
+        $target_dir = "img/upload/";
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+        
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);    
+        if($check !== false) {
+                
+            $uploadOk = 1;
+
+        } else {
+                
+            echo "Hiba: A fájl nem kép! ";
+            $uploadOk = 0;
+        }
+    
+        if ($uploadOk == 0) {
+        
+            echo "Hiba: A fájl nem lett feltöltve!";
+    
+        } else {
+        
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+
+                echo "<div style='z-index: -1'>" . basename( $_FILES["fileToUpload"]["name"]) . " fel lett töltve! Köszönjük a képet!</div>";    
+            } else {
+                
+                echo "Hiba: Feltöltés közben hiba lépett fel!";
+            }
+        }
+    }
+
+    public function insertPerson($name, $age, $title, $House_name, $picture="unknown.PNG") {
+        
+        $sql = "INSERT INTO person (name, age, title, House_name, picture) 
+        VALUES ($name, $age, $title, $House_name, $picture)";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
     }
 }
 
