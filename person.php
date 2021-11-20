81f1f2744fa62022158a8c4bc7497cd759db306e
Pozsgai Barnabás <h050515@stud.u-szeged.hu>
2021-11-20 17:08:23 +0100

    bbb

--- person.php
+++ person.php
@@ -23,17 +23,35 @@ if (isset($_POST['back'])) {
     <div class="content">
         <div class="container">
             <div class="fixed">
-                <img src="<?php echo "img/" . $personData["picture"] ?>" alt="Avatar">
+                <img src="<?php echo "img/" . $personData["person_picture"] ?>" alt="Avatar">
             </div>
             <div class="flex-item">
-                <h2><?php echo $personData["name"] ?></h2>
-                <p>
+                <div class="inline">
+                    <div><?php echo $personData["person_name"] ?></div>
+                    <img src="<?php echo "img/" . $personData["house_picture"] ?>" id="house_picture">
+                </div>
                     <?php 
-                        echo " age: " . $personData["age"] .  "<br />" .
-                            " title: " . $personData["title"] .  "<br />" .
-                            " House name: " . $personData["House_name"];
+
+                    /*
+                    person.name AS person_name, 
+                        person.age AS person_age, 
+                        person.title AS person_title, 
+                        person.picture AS person_picture, 
+                        house.name AS house_name, 
+                        house.symbol AS house_symbol, 
+                        house.picture AS house_picture
+                    
+                    */
+
+                        echo "<dl>";
+                            echo "<dt>Age:</dt>";
+                            echo "<dd>" . $personData["person_age"] .  "</dd>";
+                            echo "<dt>Title:</dt>";
+                            echo "<dd>" . $personData["person_title"] .  "</dd>";
+                            echo "<dt>House name:</dt>";
+                            echo "<dd>" . $personData["house_name"] .  "</dd>";
+                        echo "</dl>";
                     ?>
-                </p>
                 <form method="POST">
                     <input type="submit" value="Back" name="back">
                 </form>
@@ -71,7 +89,20 @@ class Person {
 
     public function getPersonDataById($id) {
         
-        $sql = "SELECT * FROM person WHERE id =" . $id;
+        $sql = "SELECT 
+            person.name AS person_name, 
+            person.age AS person_age, 
+            person.title AS person_title, 
+            person.picture AS person_picture, 
+            house.name AS house_name, 
+            house.symbol AS house_symbol, 
+            house.picture AS house_picture
+                FROM person, house , member_of_house
+                WHERE 
+                    person.id = member_of_house.Person_id AND
+                    member_of_house.House_id = house.id AND
+                    person.id =" . $id;
+        
         $result = $this->conn->query($sql);
 
         if ($result->num_rows > 0) {
